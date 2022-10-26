d59cffd9899b3d87cb47b0ad3adefec7c861e1ef
saitharun996 <saitharun996@gmail.com>
2022-10-26 15:00:05 +0530

    updated

--- RestAssured/src/test/java/com/rmgyantra/crud_DIfferent_ways_to_post_request/CreatingProjectUsing_POJOClassTest.java
+++ RestAssured/src/test/java/com/rmgyantra/crud_DIfferent_ways_to_post_request/CreatingProjectUsing_POJOClassTest.java
@@ -1,5 +1,6 @@
 package com.rmgyantra.crud_DIfferent_ways_to_post_request;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.rgmyantra.POJO.POJOClass;
@@ -10,10 +11,10 @@ import static io.restassured.RestAssured.*;
 
 public class CreatingProjectUsing_POJOClassTest
 {
-	@Test
-	public void creatingProject()
+	@Test(dataProvider="provideData")
+	public void creatingProject(String createdBy, String projectName, String status, int teamSize )
 	{
-		POJOClass pj=new POJOClass("Saitharun", "SDET-93456987", "On-going", 24);
+		POJOClass pj=new POJOClass(createdBy, projectName, status, teamSize);
 		
 		given()
 		.contentType(ContentType.JSON)
@@ -22,6 +23,26 @@ public class CreatingProjectUsing_POJOClassTest
 		.post("http://localhost:8084/addProject")
 		.then()
 		.log().all();
+	}
+	
+	@DataProvider
+	public Object[][] provideData()
+	{
+		Object[][] objArr=new Object[2][4];
+		
+		objArr[0][0]="Puppy12";
+		objArr[0][1]="RJNR_HOME";
+		objArr[0][2]="Created";
+		objArr[0][3]=4;
+		
+		objArr[1][0]="harsha";
+		objArr[1][1]="SDET_12ABN";
+		objArr[1][2]="Created";
+		objArr[1][3]=7;
+		
+		return objArr;
+		
+		
 	}
 
 }
